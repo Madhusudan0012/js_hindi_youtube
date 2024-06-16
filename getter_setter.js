17267109076ebe77d424c7cca78e2f2ba07b43b5
Madhusudan Singh Chauhan <136247818+Madhusudan0012@users.noreply.github.com>
2024-06-16 23:39:59 +0530

    Add files via upload

---
+++ getter_setter.js
@@ -0,0 +1,17 @@
+class user{
+    constructor(email , password){
+        this.email = email;
+        this.password = password;
+
+    }
+
+    get password(){
+        return this.password.toUpperCase()
+
+    }
+}
+
+const hitesh = new UserActivation("h@Madhusudan" , "123")
+console.log(Madhusudan.password);
+
+
